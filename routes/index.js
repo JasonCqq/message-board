35456506551f1be25552904230b3028af1965ff8
Jason <121495300+jason21715@users.noreply.github.com>
2023-06-29 01:55:31 -0700

    Add form and message submission

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,17 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "test" });
+});
+
+router.post("/new", (req, res) => {
+  messages.push({
+    text: req.body.message,
+    user: req.body.author,
+    added: new Date(),
+  });
+  res.redirect("/");
+});
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,9 @@
 var express = require("express");
 var router = express.Router();
-const path = require("path");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
-});
-
-router.get("/new", function (req, res, next) {
-  res.sendFile();
 });
 
 module.exports = router;
